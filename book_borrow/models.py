41d71f5ccd38562271c29b8c1cbdd69b9f541e8c
Shobhit Pandey <shobhit.pandey@oyorooms.com>
2023-01-13 00:41:50 +0530

    feat: model and requirement.txt updated

--- book_borrow/models.py
+++ book_borrow/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+BOOK_TYPES = (
+    ('Paperbacks', 'Paperbacks'),
+    ('Hardcover', 'Hardcover'),
+    ('Handmade', 'Handmade')
+)
+
 
 class Author(models.Model):
     auther_name = models.CharField(max_length=255, blank=False)
@@ -11,10 +17,11 @@ class Book(models.Model):
     name = models.CharField(max_length=255, blank=False)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     available = models.BooleanField(default=True)
+    type = models.CharField(max_length=50, choices=BOOK_TYPES, blank=False)
 
 
 class BookingHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    booking_time = models.DateTimeField(auto_now_add=True)
-    book_return_time = models.DateTimeField(null=True)
+    issue_time = models.DateTimeField(auto_now_add=True)
+    return_time = models.DateTimeField(null=True)
