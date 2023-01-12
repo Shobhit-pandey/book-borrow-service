3fd31fad5b2a425510c07d57ec6fedec43111891
Shobhit Pandey <shobhit.pandey@oyorooms.com>
2023-01-13 03:26:09 +0530

    fix: minor changes

--- book_borrow/views.py
+++ book_borrow/views.py
@@ -29,16 +29,16 @@ def borrow_book(request, book_id):
     user = get_temporary_user()
     book = Book.objects.filter(id=book_id).first()
     if not book:
-        return Response(status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if not book.available:
-        return Response({"message": "Book is not available"}, status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "Book is not available"}, status=status.HTTP_400_BAD_REQUEST)
     is_blocked_for_current_user = book_availability_for_current_user(
         BookDataClass.get_from_model_object(book),
         UserDataClass.get_from_model_object(user),
     )
     if is_blocked_for_current_user.blocked:
         return Response({"message": "Its seems you booked same book in near past. You can book this book after" +
-                                    str(is_blocked_for_current_user.unblock_time)}, status.HTTP_400_BAD_REQUEST)
+                                    str(is_blocked_for_current_user.unblock_time)}, status=status.HTTP_400_BAD_REQUEST)
     book.available = False
     book.save()
     BookingHistory.objects.create(book=book, user=user)
@@ -52,17 +52,17 @@ def return_book(request, book_id):
     user = get_temporary_user()
     book = Book.objects.filter(id=book_id).first()
     if not book:
-        return Response(status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if book.available:
-        return Response({"message": "This book is not issued to you"}, status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "This book is not issued to you"}, status=status.HTTP_400_BAD_REQUEST)
     booking_history = BookingHistory.objects.filter(book_id=book_id, user=user, return_time=None).first()
     if not booking_history:
-        return Response({"message": "This book is not issued to you"}, status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "This book is not issued to you"}, status=status.HTTP_400_BAD_REQUEST)
     book.available = True
     book.save()
     booking_history.return_time = timezone.now()
     booking_history.save()
-    return Response({"message": "Book is returned"}, status.HTTP_200_OK)
+    return Response({"message": "Book is returned"}, status=status.HTTP_200_OK)
 
 
 # @login_required()
@@ -72,9 +72,9 @@ def next_borrow_time(request, book_id):
     user = get_temporary_user()
     book = Book.objects.filter(id=book_id).first()
     if not book:
-        return Response(status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if not book.available:
-        return Response({"message": "This book is not available for issue"}, status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "This book is not available for issue"}, status=status.HTTP_400_BAD_REQUEST)
     is_blocked_for_current_user = book_availability_for_current_user(
         BookDataClass.get_from_model_object(book),
         UserDataClass.get_from_model_object(user),
